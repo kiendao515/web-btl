f45627c791198661e0f4b1c823caa23b84da686d
kiendao <kiendao@var-meta.com>
2023-01-10 10:49:29 +0700

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 declare const module: any;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{ cors: true });
-
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('Web API documentary')
