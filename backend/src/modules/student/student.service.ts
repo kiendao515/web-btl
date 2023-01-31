43a357baac4d6e1057a6960144ef95a373b973a0
kiendao <kiendao@var-meta.com>
2023-01-31 20:34:01 +0700

    update

--- backend/src/modules/student/student.service.ts
+++ backend/src/modules/student/student.service.ts
@@ -54,13 +54,13 @@ export class StudentService {
         if (isMatch) {
           return department;
         } else {
-          throw new Error(`password is incorrect`);
+          throw new Error(`Password is incorrect`);
         }
       } else {
-        return null;
+        throw new Error(`Email not found`);;
       }
     } catch (err) {
-      throw new Error(`Error finding ${err} user ${err.message}`);
+      throw new Error(`${err.message}`);
     }
   }
 
